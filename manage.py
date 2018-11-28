7852f5d51904397562a94f32439e363220b3ef75
Michael Brown <michael@msbrown.net>
2018-11-28 09:19:28 -0500

    Better error if .env missing

--- manage.py
+++ manage.py
@@ -8,6 +8,9 @@ if __name__ == "__main__":
     envfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
     if os.path.exists(envfile):
         environ.Env.read_env(envfile)
+    else:
+        raise FileNotFoundError('Could not find .env file. To fix, copy '
+                                '"project/develop/env" to ".env".')
 
     try:
         from django.core.management import execute_from_command_line
