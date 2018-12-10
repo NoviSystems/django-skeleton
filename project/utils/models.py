6a24a1bd9ffb3cb26e4d5144c22127eae7781e20
Andrew Brown <brownan@gmail.com>
2018-12-10 15:39:53 -0500

    Added custom User model that uses emails as login
    It's not used by default but I use it often enough I wanted to put it in the
    skeleton

--- project/utils/models.py
+++ project/utils/models.py
@@ -2,6 +2,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 # For more information, read:
-# https://docs.djangoproject.com/en/2.0/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
+# https://docs.djangoproject.com/en/2.1/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
 class User(AbstractUser):
     pass
